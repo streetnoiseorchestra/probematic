18749ad48990d27653d5a48f230a6bc90e6a5075
Casey Link <unnamedrambler@gmail.com>
2025-03-14 10:58:30 +0100

    Add datomic-pro sqlite init script

---
+++ scripts/init-datomic-sqlite.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Creating SQLite database and schema..."
+sqlite3 ./data/datomic-sqlite.db "
+      CREATE TABLE IF NOT EXISTS datomic_kvs (
+        id TEXT NOT NULL PRIMARY KEY,
+        rev INTEGER,
+        map TEXT,
+        val BLOB
+      );"
+echo "Database initialization complete."
