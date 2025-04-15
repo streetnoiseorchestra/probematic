1792660714f15c022517178a16d7f6e3c22827aa
Casey Link <unnamedrambler@gmail.com>
2025-04-15 08:33:23 +0200

    remove dir-locals.el

--- .dir-locals.el
+++
@@ -1,6 +0,0 @@
-((nil . (
-         (cider-preferred-build-tool           . clojure-cli)
-         (cider-redirect-server-output-to-repl . t)
-         (cider-repl-display-help-banner       . nil)
-         (clojure-toplevel-inside-comment-form . nil)
-         )))
