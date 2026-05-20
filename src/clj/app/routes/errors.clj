a95b86af4f659a3c282474ca811bc74e62264068
Casey Link <unnamedrambler@gmail.com>
2026-05-20 19:01:47 +0200

    remove app.ui and app.render from error pages

--- src/clj/app/routes/errors.clj
+++ src/clj/app/routes/errors.clj
@@ -2,47 +2,110 @@
   (:require
    [app.email :as email]
    [app.errors :as error.util]
-   [app.render :as render]
-   [app.ui :as ui]))
+   [app.html :as html]
+   [app.ui2 :as ui2]))
+
+(defn- error-copy [tr status]
+  (condp = status
+    401 {:code "401"
+         :title (tr [:error/unauthorized-title])
+         :message (tr [:error/unauthorized-message])}
+    404 {:code "404"
+         :title (tr [:error/not-found-title])
+         :message (tr [:error/not-found-message])}
+    {:code (str (or status 500))
+     :title (tr [:error/unknown-title])
+     :message (tr [:error/unknown-message])}))
+
+(defn- error-content [{:keys [human-id tr]} status]
+  (let [{:keys [code title message]} (error-copy tr status)]
+    [[:header {:class (when (= 500 status) "danger")}
+      [:p code]
+      [:h1 title]]
+     [:p message]
+     [:p [:code human-id]]
+     [:footer
+      [:a {:href "/"}
+       (tr [:error/go-home])]
+      [:form {:method "post"
+              :action "/notify-admin"}
+       [:input {:type "hidden"
+                :name "human-id"
+                :value human-id}]
+       [:button {:type "submit"}
+        (tr [:error/notify])]]]]))
+
+(defn- error-fragment-body [req status]
+  [:body
+   [:style (html/raw ui2/standalone-page-style)]
+   [:main {:id "main"}
+    (into [:section]
+          (error-content req status))]])
+
+(defn- html-response
+  ([status body]
+   (html-response status {} body))
+  ([status headers body]
+   {:status status
+    :headers (merge {"Content-Type" "text/html"} headers)
+    :body (html/->str body)}))
+
+(defn- error-page-response-fragment [_cause req status]
+  (html-response (or status 404)
+                 {"HX-Retarget" "body"}
+                 (error-fragment-body req status)))
+
+(defn- error-page-response [_cause req status]
+  (let [{:keys [title message]} (error-copy (:tr req) status)]
+    (apply ui2/standalone-page
+           {:status (or status 404)
+            :title title
+            :description message}
+           (error-content req status))))
+
+(defn- handle-error [ex req status]
+  (assert (map? req))
+  (error.util/log-error! req ex)
+  (error.util/send-event! req ex)
+  ;; TODO once htmx is gone, remove this
+  (if (:htmx? req)
+    (error-page-response-fragment ex req status)
+    (error-page-response ex req status)))
 
 (defn unauthorized-error [ex req]
-  (assert (map? req))
-  (error.util/log-error! req ex)
-  (error.util/send-event! req ex)
-  (if (:htmx? req)
-    (ui/error-page-response-fragment ex req 401)
-    (ui/error-page-response ex req 401)))
+  (handle-error ex req 401))
 
 (defn validation-error [ex req]
-  (assert (map? req))
-  (error.util/log-error! req ex)
-  (error.util/send-event! req ex)
-  (if (:htmx? req)
-    (ui/error-page-response-fragment ex req 400)
-    (ui/error-page-response ex req 400)))
+  (handle-error ex req 400))
 
 (defn not-found-error [ex req]
-  (assert (map? req))
-  (error.util/log-error! req ex)
-  (error.util/send-event! req ex)
-  (if (:htmx? req)
-    (ui/error-page-response-fragment ex req 404)
-    (ui/error-page-response ex req 404)))
+  (handle-error ex req 404))
 
 (defn unknown-error [ex req]
-  (assert (map? req))
-  (error.util/log-error! req ex)
-  (error.util/send-event! req ex)
-  (if (:htmx? req)
-    (ui/error-page-response-fragment ex req 500)
-    (ui/error-page-response ex req 500)))
+  (handle-error ex req 500))
+
+(defn- notify-human-id [req]
+  (or (get-in req [:params :human-id])
+      (get-in req [:params "human-id"])
+      (get-in req [:form-params :human-id])
+      (get-in req [:form-params "human-id"])))
 
 (defn notify-admin [req]
-  (let [human-id (-> req :params :human-id)
+  (let [tr (:tr req)
+        human-id (notify-human-id req)
         member (-> req :session :session/member)]
     (email/send-admin-email! req member human-id)
-    (render/snippet-response
-     [:span {:class "text-sno-orange-600"} "Notification sent!"])))
+    (if (:htmx? req)
+      (html-response 200 [:span (tr [:action/done])])
+      (ui2/standalone-page
+       {:status 200
+        :title (tr [:action/done])
+        :description (tr [:action/done])}
+       [:header
+        [:h1 (tr [:action/done])]]
+       [:footer
+        [:a {:href "/"}
+         (tr [:error/go-home])]]))))
 
 (defn routes []
   [""
