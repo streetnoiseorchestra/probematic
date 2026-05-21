3a4bdb4fa8af07502db3895e8a8044073e041142
Casey Link <unnamedrambler@gmail.com>
2026-05-21 10:34:07 +0200

    clean up zip helpers

--- test/app/util/zip_test.clj
+++ test/app/util/zip_test.clj
@@ -1,36 +1,94 @@
 (ns app.util.zip-test
   (:require
    [app.util.zip :as zip]
-   [babashka.fs :as fs]
-   [clojure.string :as str]
-   [clojure.test :refer [deftest is]])
+   [clojure.java.io :as io]
+   [clojure.test :refer [deftest is testing]])
   (:import
-   [java.util.zip ZipEntry ZipFile ZipOutputStream]))
+   [java.io ByteArrayInputStream ByteArrayOutputStream]
+   [java.nio.charset StandardCharsets]
+   [java.util.zip ZipInputStream ZipOutputStream]))
 
-(defn- zip-entries [file]
-  (with-open [zip-file (ZipFile. file)]
-    (into {}
-          (map (fn [^ZipEntry entry]
-                 [(.getName entry)
-                  (with-open [in (.getInputStream zip-file entry)]
-                    (slurp in))]))
-          (enumeration-seq (.entries zip-file)))))
+(defn- utf-8-bytes [s]
+  (.getBytes s StandardCharsets/UTF_8))
 
-(deftest temp-file-zip-test
-  (let [temp-file (zip/temp-file-zip
-                   "probematic-test."
-                   ".zip"
-                   (fn [^ZipOutputStream zip-output]
-                     (.putNextEntry zip-output (ZipEntry. "hello.txt"))
-                     (.write zip-output (.getBytes "hello" "UTF-8"))
-                     (.closeEntry zip-output)))]
-    (try
-      (is (= {:file?         true
-              :name-matches? true
-              :entries       {"hello.txt" "hello"}}
-             {:file?         (instance? java.io.File temp-file)
-              :name-matches? (and (str/starts-with? (.getName temp-file) "probematic-test.")
-                                  (str/ends-with? (.getName temp-file) ".zip"))
-              :entries       (zip-entries temp-file)}))
-      (finally
-        (fs/delete-if-exists temp-file)))))
+(defn- input-stream [s]
+  (ByteArrayInputStream. (utf-8-bytes s)))
+
+(defn- entry-content [^ZipInputStream zip-input]
+  (let [out (ByteArrayOutputStream.)]
+    (io/copy zip-input out)
+    (String. (.toByteArray out) StandardCharsets/UTF_8)))
+
+(defn- zip-stream-entries [stream]
+  (with-open [zip-input (ZipInputStream. stream)]
+    (loop [entries {}]
+      (if-let [entry (.getNextEntry zip-input)]
+        (let [entries (assoc entries (.getName entry) (entry-content zip-input))]
+          (.closeEntry zip-input)
+          (recur entries))
+        entries))))
+
+(defn- zip-bytes->entries [^ByteArrayOutputStream out]
+  (zip-stream-entries (ByteArrayInputStream. (.toByteArray out))))
+
+(deftest filename-normalization-test
+  (is (= {:de-accented "weird user input.jpeg"
+          :nil-input   nil
+          :encoded     "weird-user-input-.jpeg"
+          :safe        "safe name-01.txt"}
+         {:de-accented (zip/de-accent "wëird user înput.jpeg")
+          :nil-input   (zip/de-accent nil)
+          :encoded     (zip/encode-filename "wëird:user:înput:.jpeg")
+          :safe        (zip/encode-filename "safe name-01.txt")})))
+
+(deftest append-stream-test
+  (testing "appends and normalizes a zip entry"
+    (let [out    (ByteArrayOutputStream.)
+          result (with-open [zip-output (ZipOutputStream. out StandardCharsets/UTF_8)]
+                   (let [returned (zip/append-stream!
+                                   zip-output
+                                   "wëird:user:înput:.txt"
+                                   (input-stream "hello"))]
+                     (.finish zip-output)
+                     {:returned-same? (identical? zip-output returned)}))]
+      (is (= {:returned-same? true
+              :entries        {"weird-user-input-.txt" "hello"}}
+             (assoc result :entries (zip-bytes->entries out))))))
+  (testing "nil input is a no-op"
+    (let [out    (ByteArrayOutputStream.)
+          result (with-open [zip-output (ZipOutputStream. out StandardCharsets/UTF_8)]
+                   (let [returned (zip/append-stream! zip-output "empty.txt" nil)]
+                     (.finish zip-output)
+                     {:returned-same? (identical? zip-output returned)}))]
+      (is (= {:returned-same? true
+              :entries        {}}
+             (assoc result :entries (zip-bytes->entries out)))))))
+
+(deftest open-and-append-test
+  (let [closed? (atom false)
+        out     (ByteArrayOutputStream.)]
+    (with-open [zip-output (ZipOutputStream. out StandardCharsets/UTF_8)]
+      (let [returned (zip/open-and-append!
+                      zip-output
+                      "opened.txt"
+                      (fn []
+                        (proxy [ByteArrayInputStream] [(utf-8-bytes "opened")]
+                          (close []
+                            (reset! closed? true)
+                            (proxy-super close)))))]
+        (.finish zip-output)
+        (is (= {:returned-same? true
+                :closed?        true
+                :entries        {"opened.txt" "opened"}}
+               {:returned-same? (identical? zip-output returned)
+                :closed?        @closed?
+                :entries        (zip-bytes->entries out)}))))))
+
+(deftest piped-zip-input-stream-test
+  (is (= {"one.txt" "one"
+          "two.txt" "two"}
+         (zip-stream-entries
+          (zip/piped-zip-input-stream
+           (fn [zip-output]
+             (zip/append-stream! zip-output "one.txt" (input-stream "one"))
+             (zip/append-stream! zip-output "two.txt" (input-stream "two"))))))))
