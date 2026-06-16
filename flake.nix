342c08b231e31e983ae73220999b698a5a2b887d
Casey Link <unnamedrambler@gmail.com>
2026-06-16 11:32:15 +0200

    nix pkg: get a mkcljapp nix pkg build working

--- flake.nix
+++ flake.nix
@@ -4,14 +4,14 @@
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # tracks nixpkgs unstable branch
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
-    devenv.url = "https://flakehub.com/f/ramblurr/nix-devenv/*";
+    devenv.url = "github:ramblurr/nix-devenv";
     devenv.inputs.nixpkgs.follows = "nixpkgs";
-    clojure-nix-locker.url = "github:bevuta/clojure-nix-locker";
-    clojure-nix-locker.inputs.nixpkgs.follows = "nixpkgs";
+    clj-helpers.url = "github:outskirtslabs/clojure-nix-locker-helpers";
+    clj-helpers.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs =
     inputs@{
-      clojure-nix-locker,
+      clj-helpers,
       self,
       devenv,
       devshell,
@@ -19,92 +19,50 @@
     }:
     let
       jdk = "jdk25";
+      package =
+        pkgs:
+        let
+          jdkPackage = pkgs.${jdk};
+          clojure = pkgs.clojure.override { jdk = jdkPackage; };
+        in
+        clj-helpers.lib.mkCljApp {
+          inherit pkgs;
+          modules = [
+            {
+              jdk = jdkPackage;
+              name = "probematic";
+              version = "0.0.TODO";
+              src = ./.;
+              buildCommand = "clojure -Srepro -T:build uberjar";
+              lockCommand = ''
+                export HOME="$tmp/home"
+                export GITLIBS="$HOME/.gitlibs"
+                export JAVA_TOOL_OPTIONS="-Duser.home=$HOME"
+                unset CLJ_CACHE CLJ_CONFIG XDG_CACHE_HOME XDG_CONFIG_HOME XDG_DATA_HOME
+                export JAVA_HOME="${jdkPackage.home}"
+                export JAVA_CMD="${jdkPackage}/bin/java"
+                export GIT_REV="lockfile-generation"
+                export PATH="${clojure}/bin:${jdkPackage}/bin:$PATH"
+
+                clojure -Srepro -X:deps prep :aliases '[:dev :kaocha]'
+                clojure -Srepro -P -M:dev:kaocha
+                clojure -Srepro -T:build uberjar
+              '';
+              gitRev = clj-helpers.lib.gitRev self;
+            }
+          ];
+        };
     in
     devenv.lib.mkFlake ./. {
       inherit inputs;
+      pname = "probematic";
       withOverlays = [
         devshell.overlays.default
         devenv.overlays.default
       ];
       packages = {
-        default =
-          pkgs:
-          let
-            jdkPackage = pkgs.${jdk};
-            lockerPkgs = pkgs // {
-              clojure = pkgs.clojure.override { jdk = jdkPackage; };
-            };
-            clojure = pkgs.clojure.override { jdk = jdkPackage; };
-            gitRev =
-              if self ? rev then
-                self.rev
-              else if self ? dirtyRev then
-                self.dirtyRev
-              else
-                "dirty";
-            clojureLocker = (import "${clojure-nix-locker}/default.nix" { pkgs = lockerPkgs; }).lockfile {
-              src = ./.;
-              lockfile = "./deps-lock.json";
-              extraPrepInputs = [ pkgs.git ];
-            };
-          in
-          pkgs.stdenv.mkDerivation {
-            pname = "TODO";
-            version = "0.0.TODO";
-            src = ./.;
-            nativeBuildInputs = [
-              clojure
-              pkgs.coreutils
-              pkgs.findutils
-              pkgs.git
-              jdkPackage
-            ];
-            GIT_REV = gitRev;
-            JAVA_HOME = jdkPackage.home;
-            buildPhase = ''
-              runHook preBuild
-
-              source ${clojureLocker.shellEnv}
-              export JAVA_HOME="${jdkPackage.home}"
-              export JAVA_CMD="${jdkPackage}/bin/java"
-
-              clojure -Srepro -M:dev:kaocha
-              clojure -Srepro -T:build jar
-
-              runHook postBuild
-            '';
-            installPhase = ''
-              runHook preInstall
-
-              mkdir -p $out
-              cp "$(find target -type f -name '*.jar' -print | head -n 1)" $out/
-
-              runHook postInstall
-            '';
-          };
-        locker =
-          pkgs:
-          let
-            jdkPackage = pkgs.${jdk};
-            lockerPkgs = pkgs // {
-              clojure = pkgs.clojure.override { jdk = jdkPackage; };
-            };
-            clojure = pkgs.clojure.override { jdk = jdkPackage; };
-            clojureLocker = (import "${clojure-nix-locker}/default.nix" { pkgs = lockerPkgs; }).lockfile {
-              src = ./.;
-              lockfile = "./deps-lock.json";
-              extraPrepInputs = [ pkgs.git ];
-            };
-          in
-          clojureLocker.commandLocker ''
-            export HOME="$tmp/home"
-            export GITLIBS="$tmp/home/.gitlibs"
-            unset CLJ_CACHE CLJ_CONFIG XDG_CACHE_HOME XDG_CONFIG_HOME XDG_DATA_HOME
-
-            ${clojure}/bin/clojure -Srepro -X:deps prep :aliases "[:dev :kaocha]"
-            ${clojure}/bin/clojure -Srepro -P -M:dev:kaocha
-            ${clojure}/bin/clojure -Srepro -P -T:build jar
-          '';
+        default = package;
+        locker = pkgs: (package pkgs).locker;
       };
       devShell =
         pkgs:
@@ -121,7 +79,12 @@
             pkgs.lightningcss
             pkgs.watchexec
             pkgs.gitleaks
-            (if self ? packages then self.packages.${pkgs.system}.locker else pkgs.deps-lock)
+            (
+              if self ? packages then
+                self.packages.${pkgs.system}.locker
+              else
+                clj-helpers.packages.${pkgs.system}.deps-lock
+            )
             # pkgs.foobar
           ];
 
