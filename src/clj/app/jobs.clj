b07ec3209bb5d884c3e5ee561469546304b6705a
Casey Link <unnamedrambler@gmail.com>
2026-09-22 12:38:49 +0200

    jobs: remove unused ping schedule

--- src/clj/app/jobs.clj
+++ src/clj/app/jobs.clj
@@ -4,13 +4,11 @@
    [app.jobs.sync-songs :refer [make-songs-sync-job]]
    [app.jobs.probe-housekeeping :refer [make-probe-housekeeping-job]]
    [app.jobs.poll-housekeeping :refer [make-poll-housekeeping-job]]
-   [app.jobs.reminders :refer [make-reminder-job]]
-   [app.jobs.ping :refer [make-job-ping]]))
+   [app.jobs.reminders :refer [make-reminder-job]]))
 
 ;; Add your job constructors here
 (defn job-defs [opts]
-  {:job/ping               make-job-ping
-   :job/probe-housekeeping (make-probe-housekeeping-job opts)
+  {:job/probe-housekeeping (make-probe-housekeeping-job opts)
    :job/sync-songs         (make-songs-sync-job opts)
    :job/poll-housekeeping  (make-poll-housekeeping-job opts)
    :job/reminders          (make-reminder-job opts)
